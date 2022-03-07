8299c76797c1a95dcdd898b4ec15e4409a7f6099
Daniel Ocampo <ochampo77@gmail.com>
2022-03-06 22:27:23 -1000

    Created a consumer that gets the messages.

---
+++ FanOutConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+IConnection conn;
+IModel channel;
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "192.168.2.2";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "docampo";
+factory.Password = "Flowers!@#4";
+
+conn = factory.CreateConnection();
+
+channel = conn.CreateModel();
+
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += Consumer_Recevied;
+
+var consumerTag = channel.BasicConsume("my.Queue1",true,consumer);
+Console.WriteLine("Waiting for messages> press any key to exit.");
+Console.ReadKey();
+
+ static void Consumer_Recevied(object sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body.Span;
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("[x] Received {0}", message);
+}
